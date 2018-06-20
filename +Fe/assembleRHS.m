Mathias-Scheunert/2d_lagrange

commit 892849f17804f64c6004082c2d12448376562f52

Add more exact point source representation 	- revise assembling of rhs 	- add new source type to DRIVE

+Fe/assembleRHS.m
@@ -38,8 +38,94 @@ function b = assembleRHS(fe, mesh, TX, verbosity)
        fprintf('Assemble rhs ... '); 
     end
     
-    % The assembling of the rhs vector follows the same procedure as
-    % assembling the mass matrix.
+    switch TX.type
+        case {'homogeneous', 'point_approx'}
+            getRHS = @getFunctionRHS;
+            
+        case 'point_exact'
+            getRHS = @getDistributionRHS;
+            
+        otherwise
+            error('Unknown soure type.');
+    end
+    
+    b = getRHS(fe, mesh, TX);
+    
+    if verbosity
+       fprintf('done.\n'); 
+    end
+end
+
+function b = getDistributionRHS(fe, mesh, TX)
+    % The assembling of the rhs vector for a distribution rhs (Dirac) 
+    % follows the same procedure as assembling the interpolation operator.
+    % I.e. the evaluation of basisfunctions at the source location whereas
+    % the source strengh acts as scaling.
+
+    % Get common sizes.
+    n_point = size(TX.coo, 1);
+    
+    % Try to identify cells belonging to TX.pos by matlab builtin.
+    cell_idx = tsearchn(mesh.vertices, mesh.cell2vtx, TX.coo);
+    cell_idx_fail = isnan(cell_idx);
+    cell_idx_fail = find(cell_idx_fail);
+    n_cell_idx_fail = length(cell_idx_fail);
+    if n_cell_idx_fail ~= 0
+        warning('tsearchn failed to identify cells for some RX points.');
+    end
+   
+    for ii = 1:n_cell_idx_fail
+        % Use 'own' functionality to obtain cell index for corrupt points.
+        % Get point coordinates w.r.t. the reference simplex.
+        maps_fail = arrayfun(@(x) ...
+        {(Mesh.getAffineMap(x, mesh, TX(cell_idx_fail(ii),:)))}, ...
+        (1:fe.sizes.cell).');
+
+        % Check if point(s) is/are inside simplex.
+        tol = pick(2, 0, eps * 1e1);
+        cells_fit = cell2mat(cellfun(@(x) {(...
+            all(x.xy_ref > -tol, 2) & ...
+            all(x.xy_ref <= 1 + tol, 2) & ...
+            (sum(x.xy_ref, 2) - 1 < tol)).'}, ...
+            maps_fail));
+    
+        % Obtain cell indices w.r.t to each TX point.
+        % (For multiple hits just take the first cell)
+        cell_idx(cell_idx_fail(ii)) = find(cells_fit, 1, 'first');
+        if isempty(cell_idx(cell_idx_fail(ii)))
+            error('No cell for observation point could be found.');
+        end
+    end
+    
+    % Get DOF index for respective cells.
+    cell_idx = num2cell(cell_idx);
+    cells2DOF = cell2mat(cellfun(@(x) {fe.DOF_maps.cell2DOF{x}.'}, cell_idx)).';
+    
+    % Get affine maps for all found cells w.r.t. the respective points.
+    maps = cellfun(@(x, y) {Mesh.getAffineMap(x, mesh, y)}, ...
+        cell_idx, mat2cell(TX.coo, ones(n_point, 1), 2));
+    
+    % Evaluate basis functions at TX point(s) and scale it.
+    cur_base = zeros(fe.sizes.DOF_loc, n_point);
+    for kk = 1:n_point
+        cur_x_ref = maps{kk}.xy_ref(1);
+        cur_y_ref = maps{kk}.xy_ref(2);
+        cur_base(:, kk) = TX.val(kk) * fe.base.Phi(cur_x_ref, cur_y_ref).';
+    end
+    
+    % Set up rhs vector for the linear combination of respective 
+    % basis functions.
+    n_DOF = fe.sizes.DOF;
+    n_DOF_loc = fe.sizes.DOF_loc;
+    i = cells2DOF(:);
+    j = ones(n_point * n_DOF_loc, 1);
+    s = cur_base(:);
+    b = sparse(i, j, s, n_DOF, 1);
+end
+
+function b = getFunctionRHS(fe, mesh, TX)
+    % The assembling of the rhs vector for a function-like rhs follows the 
+    % same procedure as assembling the mass matrix.
     % I.e. the numerical evaluation of an integral over the single
     % elements/cells is required, whereas the product of the basis
     % functions with a function (describing the source) forms the integral
@@ -92,8 +178,4 @@ function b = assembleRHS(fe, mesh, TX, verbosity)
     
     % Create sparse rhs vector.
     b = sparse(i, 1, s, n_DOF_glob, 1);
-    
-    if verbosity
-       fprintf('done.\n'); 
-    end
 end

+RefSol/getPeakFunction.m
@@ -1,43 +1,55 @@
-function Peak = getPeakFunction(eps_peak, pos)
+function peak_fun = getPeakFunction(val, pos, eps_peak)
     % Get information for 2D peak function which simulates Dirac.
+    %
+    % Arbitrarily modify shape of the peak.
+    % The peak should comprise a somehow small area compared to h.
     %
     % SYNTAX
     %
-    %   Peak = getPeakFunction(eps_peak)
+    %   peak_fun = getPeakFunction(eps_peak)
     %
     % INPUT PARAMETER
-    %   eps_peak ... Scalar, denoting the amplitude and the width of the
+    %   val      ... Scalar [or vector] scaling factor[s], denoting source 
+    %                strength.
+    %   pos      ... Vector [or matrix], providing the peak position[s].
+    %   eps_peak ... Scalar, controlling the amplitude and the width of the
     %                normal distribution function which is used to 
     %                approximate the Dirac impulse.
-    %   pos      ... Vector, providing the peak position.
     %
     % OUTPUT PARAMETER
     %
-    %   Peak ... Struct, containing function handle.
+    %   peak_fun ... Struct, containing function handle.
     %
     % REMARKS
     %   See https://de.wikipedia.org/wiki/Mehrdimensionale_Normalverteilung
+    %
+    % TODO: automatically determine scaling factors w.r.t. the cell sizes.
     
     %% Check input
     
+    assert(isscalar(val) || isvector(val), ...
+        'val - scalar [or vector], denoting the amplitude[s], expected.');
+    assert((isvector(pos) && length(pos) == 2) || ...
+        (ismatrix(pos) && size(pos, 2) == 2), ...
+        'pos - vector [or matrix], denoting the peak position[s], expected.');
     assert(isscalar(eps_peak), ...
-        'eps_peak - scalar, denoting the amplitude, expected.');
-    assert(isvector(pos) && length(pos) == 2, ...
-        'pos - vector, denoting the peak position, expected.');
+        'eps_peak - scalar, controlling the width, expected.');
+    assert(length(val) == size(pos, 1), ...
+        'Length of val has to coincide with rows in pos.')
     
     %% Define peak-fuction.
-    
-    % Arbitrarily modify shape of the peak.
-    % The peak should comprise a somehow small area and have the desired
-    % amplitude.
-    % TODO: automatically determine scaling factors w.r.t. the cell sizes.
-    scale_C = eps_peak * 1e-4;
-    scale_f = scale_C ^ 1.65;
+        
+    % Get sizes.
+    n_peak = length(val);
     
     % Define function (assuming a uniform covariance matrix).
-    cord = @(x, y) [x - pos(1); y - pos(2)];
-    C = scale_C * eye(2, 2);
-    CI = 1/scale_C * eye(2, 2);
-    Peak.f = @(x, y) scale_f * 1/(2 * pi * det(C)) * ...
-        exp(-(cord(x,y).' * CI * cord(x,y)) / 2);
+    C = eps_peak * eye(2, 2);
+    CI = 1/eps_peak * eye(2, 2);
+    peak_fun.f = @(x, y) 0;
+    for ii = 1:n_peak
+        cord = @(x, y) [x - pos(ii, 1); y - pos(ii, 2)];
+        peak_fun.f = @(x, y) peak_fun.f(x,y) +  ...
+            val(ii) * (1/(2 * pi * det(C)) * ...
+            exp(-(cord(x,y).' * CI * cord(x,y)) / 2));
+    end 
 end

DRIVE_Poisson.m
@@ -52,16 +52,24 @@ RX = pick(1, ...
 RX = scale * RX;
 
 % Define source point and strength.
-[Tx, Txp, Txh] = deal(struct());
-TXp.type = 'point';
+[TXp, TXd, TXq, TXh] = deal(struct());
+TXp.type = 'point_exact';
 TXp.coo = scale * pick(2, [.1, .3], [0, 0]);
-TXp.val = 1; % discrete    Poisson problem
-TXp.ref_sol = RefSol.getPeakFunction(TXp.val, TXp.coo);
+TXp.val = 1;                  % discrete    Poisson problem (pole)
+TXp.ref_sol = RefSol.getPeakFunction(TXp.val, TXp.coo, 1e-5);
+TXd.type = 'point_exact';
+TXd.coo = scale * pick(2, [.1, .3; -0.2, -0.2], [-0.5, -0.5; 0.5,  0.5]);             
+TXd.val = [1, -1];            % discrete    Poisson problem (dipole)
+TXd.ref_sol = RefSol.getPeakFunction(TXd.val, TXd.coo, 1e-5);
+TXq.type = 'point_approx';
+TXq.coo = scale * [0.5, 0.5; -0.5, -0.5; -0.5, 0.5; 0.5, -0.5];
+TXq.val = [1, 0.5, -1, -0.5]; % discrete    Poisson problem (quadrupole)
+TXq.ref_sol = RefSol.getPeakFunction(TXq.val, TXq.coo, 1e-5);
 TXh.type = 'reference';
-TXh.val = 1; % homogeneous Poisson problem
+TXh.val = 1;                  % homogeneous Poisson problem
 TXh.ref_sol = RefSol.getConstFunction(TXh.val);
-%
-TX = pick(1, TXp, TXh);
+%              1    2    3    4
+TX = pick(2, TXp, TXd, TXq, TXh);
 
 % Define boundary conditions.
 % ([in]homogeneous Dirichlet conditions,pequal for all four sides)
@@ -82,7 +90,7 @@ y = scale * [-1, 1];
 mesh_type = pick(2, 'rhomb', 'cube');
 
 % Set number of grid refinements.
-ref_steps = 4;
+ref_steps = 3;
 
 % Set up order of Lagrange elements.
 order = pick(2, 1, 2);

commit ddb4b72247ebda6b048866f7973bd19cec3d9829

Fix bugs introduced in 892849f

+Fe/assembleRHS.m
@@ -39,7 +39,7 @@ function b = assembleRHS(fe, mesh, TX, verbosity)
     end
     
     switch TX.type
-        case {'homogeneous', 'point_approx'}
+        case {'reference', 'point_approx'}
             getRHS = @getFunctionRHS;
             
         case 'point_exact'

+RefSol/getSinFunction.m
@@ -14,7 +14,7 @@ function sin_fun = getSinFunction()
     %% Define fuction.
 
     sin_fun.f = @(X, Y) -10 * X .^ 2 + 10 * Y .^ 2 + ...
-        4 * sin_fun(X .* Y) - 2 * X + X.^ 4;
+        4 * sin(X .* Y) - 2 * X + X.^ 4;
 
     %% Define derivatives.
 
@@ -26,13 +26,13 @@ function sin_fun = getSinFunction()
     sin_fun.J = @(X, Y) [Jx(X, Y), Jy(X, Y)];
 
     % Define Hessian terms.
-    Hxx = @(X, Y) -20 - 4 * Y .^ 2 .* sin_fun(X .* Y) + 12 * X .^ 2; 
-    Hxy = @(X, Y) 4 * cos(X .* Y) - 4 * X .* Y .* sin_fun(X .* Y);
-    Hyx = @(X, Y) 4 * cos(X .* Y) - 4 * X .* Y .* sin_fun(X .* Y);
-    Hyy = @(X, Y) 20 - 4 * X .^ 2 .* sin_fun(X .* Y);
+    Hxx = @(X, Y) -20 - 4 * Y .^ 2 .* sin(X .* Y) + 12 * X .^ 2; 
+    Hxy = @(X, Y) 4 * cos(X .* Y) - 4 * X .* Y .* sin(X .* Y);
+    Hyx = @(X, Y) 4 * cos(X .* Y) - 4 * X .* Y .* sin(X .* Y);
+    Hyy = @(X, Y) 20 - 4 * X .^ 2 .* sin(X .* Y);
 
     % Summarize Hessian matrix and -Laplace.
     sin_fun.H = @(X, Y) [Hxx(X, Y), Hxy(X, Y); ...
-                     Hyx(X, Y), Hyy(X, Y)];
+                         Hyx(X, Y), Hyy(X, Y)];
     sin_fun.L = @(X, Y) -(Hxx(X, Y) + Hyy(X, Y));
 end