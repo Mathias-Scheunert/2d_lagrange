bd85a14466764b1cbe4eca9bc8cdaff453054921
Mathias Scheunert <mathias.scheunert.geophysik@gmail.com>
2018-07-04 11:47:06 +0200

    Restructured determination of bndDOF 	- preparation to make the code more flexible in handling 	  external meshes (e.i. bnd info) 	- coherent parts of bnd are stored in seperate cells now

--- +Fe/getBndDOF.m
+++ +Fe/getBndDOF.m
@@ -1,5 +1,5 @@
 function bnd = getBndDOF(fe, mesh)
-    % Provides all DOF belonging to boundaries.
+    % Provides all DOF belonging to boundaries of a rectangular domain.
     %
     % SYNTAX
     %   fe = treatDirichlet(fe, mesh, sol, bnd)
@@ -16,16 +16,17 @@ function bnd = getBndDOF(fe, mesh)
     % REMARKS
     %   Note, that the corner points occur mutiply as each of them is 
     %   shared by two edges!
-    %   See also: Fe.treatDirichlet, Fe.treatNeumann
-    % TODO: it may be wise to fix that multiply occurence!
-    %   Note that obtaining the bnd DOF is only valid for rectangle domains
-    %   as provided by Mesh.createRhombMesh and Mesh.createUnitCubeMesh!
+    %
+    %   So the following convention is used:
+    %   top & bottom: includes the outermost left and right
+    %       nodes/vertices/corner points.
+    %   left & rigth: includes all DOF at the boundary without the corners
     
     %% Check input.
     
     assert(isstruct(fe) && all(isfield(fe, {'order', 'sizes'})), ...
         'fe - struct, including all information of FE linear system, expected.');
-    assert(isstruct(mesh) && all(isfield(mesh, {'cell2vtx', 'edge2vtx'})), ...
+    assert(isstruct(mesh) && all(isfield(mesh, {'vertices', 'edge2vtx', 'bnd_edge_bot'})), ...
         'mesh - appended struct, including edge and mapping information, expected.');
     assert(any(strcmp(mesh.type, {'cube', 'rhomb'})), ...
         'mesh.type is not supported. Bnd information has to obtained from external.');
@@ -37,6 +38,8 @@ function bnd = getBndDOF(fe, mesh)
     
     % w.r.t. vertices.
     % Excluding vertex index from the already identified bnd edge list.
+    % Note, that vertex indices occure multiply (as two adjacent domain
+    % edges share the corner vertex)!
     idx_bot_vtx = mesh.edge2vtx(mesh.bnd_edge_bot, :);
     idx_bot_vtx = unique(idx_bot_vtx(:));
     idx_top_vtx = mesh.edge2vtx(mesh.bnd_edge_top, :);
@@ -45,10 +48,18 @@ function bnd = getBndDOF(fe, mesh)
     idx_left_vtx = unique(idx_left_vtx(:));
     idx_right_vtx = mesh.edge2vtx(mesh.bnd_edge_right, :);
     idx_right_vtx = unique(idx_right_vtx(:));
-    % Note, that vertex indices occure multiply (as two adjacent domain
-    % edges share the corner vertex)!
-    n_bnd_vtx_full = length(idx_bot_vtx) + length(idx_top_vtx) + ...
-        length(idx_left_vtx) + length(idx_right_vtx);
+    
+    % Identify corner point w.r.t vtx identifier.
+    vtx_lb =  intersect(idx_left_vtx, idx_bot_vtx);
+    vtx_rb =  intersect(idx_right_vtx, idx_bot_vtx);
+    vtx_lt =  intersect(idx_left_vtx, idx_top_vtx);
+    vtx_rt =  intersect(idx_right_vtx, idx_top_vtx);
+    
+    % Remove corner vertices from left and right bnd.
+    remove_idx_left = (idx_left_vtx == vtx_lb) | (idx_left_vtx == vtx_lt);
+    remove_idx_right = (idx_right_vtx == vtx_rb) | (idx_right_vtx == vtx_rt);
+    idx_left_vtx(remove_idx_left) = [];
+    idx_right_vtx(remove_idx_right) = [];
     
     switch fe.order
         case 1
@@ -56,21 +67,24 @@ function bnd = getBndDOF(fe, mesh)
             bnd_DOF_bot = idx_bot_vtx;
             bnd_DOF_top = idx_top_vtx;
             bnd_DOF_left = idx_left_vtx;
-            bnd_DOF_right = idx_right_vtx;  
-            
+            bnd_DOF_right = idx_right_vtx; 
+            [idx_bot_edg, idx_top_edg, idx_left_edg, idx_right_edg] = ...
+                deal([]);
+
         case 2
             % w.r.t. edges.
-            % Get edge index from the already identified bnd edge list.
-            idx_bot_edg = find(mesh.bnd_edge_bot);
-            idx_top_edg = find(mesh.bnd_edge_top);
-            idx_left_edg = find(mesh.bnd_edge_left);
-            idx_right_edg = find(mesh.bnd_edge_right);
+            % Get edge index from the already identified bnd edge list and
+            % increase the index w.r.t. the number of vertices.
+            idx_bot_edg = n_vtx + find(mesh.bnd_edge_bot);
+            idx_top_edg = n_vtx + find(mesh.bnd_edge_top);
+            idx_left_edg = n_vtx + find(mesh.bnd_edge_left);
+            idx_right_edg = n_vtx + find(mesh.bnd_edge_right);
             
             % Summarize.
-            bnd_DOF_bot = [idx_bot_vtx; n_vtx + idx_bot_edg];
-            bnd_DOF_top = [idx_top_vtx; n_vtx + idx_top_edg];
-            bnd_DOF_left = [idx_left_vtx; n_vtx + idx_left_edg];
-            bnd_DOF_right = [idx_right_vtx; n_vtx + idx_right_edg];
+            bnd_DOF_bot = [idx_bot_vtx; idx_bot_edg];
+            bnd_DOF_top = [idx_top_vtx; idx_top_edg];
+            bnd_DOF_left = [idx_left_vtx; idx_left_edg];
+            bnd_DOF_right = [idx_right_vtx;  idx_right_edg];
 
         otherwise
             error('Unsupported order of Lagrangian elements.');  
@@ -78,35 +92,29 @@ function bnd = getBndDOF(fe, mesh)
     
     % Summarize.
     bnd = struct();
-    bnd.DOF_bot = bnd_DOF_bot; 
-    bnd.DOF_top = bnd_DOF_top; 
-    bnd.DOF_left = bnd_DOF_left;
-    bnd.DOF_right = bnd_DOF_right;
-    bnd.bnd_DOF = [bnd_DOF_bot; bnd_DOF_top; bnd_DOF_left; bnd_DOF_right];
-    bnd.n_bnd_DOF_unique = length(unique(bnd.bnd_DOF));
+    bnd.bnd_DOF = {bnd_DOF_bot; bnd_DOF_top; bnd_DOF_left; bnd_DOF_right};
+    bnd_DOF = vertcat(bnd.bnd_DOF{:});
     inner_DOF = (1:fe.sizes.DOF).';
-    inner_DOF(bnd.bnd_DOF) = [];
+    inner_DOF(bnd_DOF) = [];
     bnd.inner_DOF = inner_DOF;
-    
-    %% Add corner point w.r.t vtx identifier.
-    
-    bnd.vtx_lb =  intersect(idx_left_vtx, idx_bot_vtx);
-    bnd.vtx_rb =  intersect(idx_right_vtx, idx_bot_vtx);
-    bnd.vtx_lt =  intersect(idx_left_vtx, idx_top_vtx);
-    bnd.vtx_rt =  intersect(idx_right_vtx, idx_top_vtx);
-    
+    bnd.n_inner_DOF = length(bnd.inner_DOF);
+    bnd.n_bnd_DOF = fe.sizes.DOF - bnd.n_inner_DOF;
+        
     %% Map bnd-DOF to their coordinates.
     
-    % Collect all DOF positions from vtx or edge midpoint coordinates.
-    n_bnd_DOF_full = length(bnd.bnd_DOF);
-    bnd_vtx = bnd.bnd_DOF <= fe.sizes.vtx;
-    bnd_edge = bnd.bnd_DOF > fe.sizes.vtx;
-    bnd_DOF_coo = zeros(n_bnd_DOF_full, 2);
-    bnd_DOF_coo(bnd_vtx,:) = mesh.vertices(bnd.bnd_DOF(bnd_vtx),:);
-    bnd_DOF_coo(bnd_edge,:) = cell2mat(cellfun(@mean, ...
-        mesh.edge2cord(bnd.bnd_DOF(bnd_edge) - fe.sizes.vtx), ...
-        'UniformOutput', false));
-        
-    % Summarize.
-    bnd.bnd_DOF_coo = bnd_DOF_coo;
+    % Collect all DOF positions from vtx or edge midpoint coordinates.    
+    bnd.bnd_DOF_coo = {...
+    [mesh.vertices(idx_bot_vtx, :); cell2mat(cellfun(@mean, ...
+        mesh.edge2cord(idx_bot_edg - fe.sizes.vtx), ...
+        'UniformOutput', false))];
+    [mesh.vertices(idx_top_vtx, :); cell2mat(cellfun(@mean, ...
+        mesh.edge2cord(idx_top_edg - fe.sizes.vtx), ...
+        'UniformOutput', false))];
+    [mesh.vertices(idx_left_vtx, :); cell2mat(cellfun(@mean, ...
+        mesh.edge2cord(idx_left_edg - fe.sizes.vtx), ...
+        'UniformOutput', false))];
+    [mesh.vertices(idx_right_vtx, :); cell2mat(cellfun(@mean, ...
+        mesh.edge2cord(idx_right_edg - fe.sizes.vtx), ...
+        'UniformOutput', false))]...
+        };
 end
