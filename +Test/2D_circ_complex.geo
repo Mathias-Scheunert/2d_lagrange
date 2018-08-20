f7baf6c5cb4016fe7b675f7829f394e8cbf52b20
Mathias Scheunert <mathias.scheunert.geophysik@gmail.com>
2018-08-20 14:16:03 +0200

    Add test script for gmsh import
        - add gmsh .geo and .msh file

---
+++ +Test/2D_circ_complex.geo
@@ -0,0 +1,95 @@
+// Gmsh project created on Mon Aug 20 09:01:27 2018
+SetFactory("OpenCASCADE");
+
+// Define geometries.
+Circle(1) = {0, 0, 0, 10, 0, 2*Pi};
+Line Loop(1) = {1};
+Plane Surface(1) = {1};
+
+Circle(2) = {0, 0, 0, 2.5, 0, 2*Pi};
+Line Loop(2) = {2};
+Plane Surface(2) = {2};
+
+Rectangle(3) = {-11, 0, 0, 22, 11, 0};
+
+BooleanDifference{ Surface{1}; Delete;}{ Surface{3};}
+BooleanDifference{ Surface{2}; Delete;}{ Surface{3}; Delete;}
+BooleanDifference{ Surface{1}; Delete;}{ Surface{2};}
+
+Rotate {{1, 0, 0}, {0, 0, 0}, -Pi/2} {
+  Surface{1,2}; 
+}
+
+Point(15) = {1, 0, 1, 0.05};
+Point{15} In Surface{2};
+
+// Define physical entities.
+Physical Line("surface") = {15, 26, 28};
+Physical Line("subsurface") = {27};
+Physical Line("interior") = {12};
+
+Physical Surface("1e2") = {2};
+Physical Surface("1e-2") = {1};
+
+// Add some complexity to model.
+
+Point(16) = {-4.7, -0, 3.6, 2.0};
+Point(17) = {-4.3, -0, 2.8, 2.0};
+Point(18) = {-3.7, -0, 2.6, 2.0};
+Point(19) = {-3.3, 0, 2.8, 2.0};
+Point(20) = {-3.2, 0, 3.1, 2.0};
+Point(21) = {-2.8, 0, 3.1, 2.0};
+Point(22) = {-2.7, 0, 3.3, 2.0};
+Point(23) = {-2.6, 0, 3.8, 2.0};
+Point(24) = {-2.8, 0, 4, 2.0};
+Point(25) = {-3.9, 0, 3.8, 2.0};
+
+Line(16) = {16, 17};
+Line(17) = {17, 18};
+Line(18) = {18, 19};
+Line(19) = {19, 20};
+Line(20) = {20, 21};
+Line(21) = {21, 22};
+Line(22) = {22, 23};
+Line(23) = {23, 24};
+Line(24) = {24, 25};
+Line(25) = {25, 16};
+
+Line Loop(3) = {17, 18, 19, 20, 21, 22, 23, 24, 25, 16};
+Plane Surface(3) = {3};
+BooleanDifference{ Surface{1}; Delete;}{ Surface{3}; Delete;}
+
+Physical Line("tunnel") = {17, 18, 20, 19, 22, 23, 24, 25, 16, 21};
+
+Point(28) = {0, 0, 0, 1.0};
+Point(29) = {5, 0, 0, 1.0};
+
+Field[1] = Attractor;
+Field[1].NodesList = {28, 29};
+
+Field[2] = Threshold;
+Field[2].IField = 1;
+Field[2].LcMin = 0.1;
+Field[2].DistMin = 0;
+Field[2].DistMax = 0.2;
+Background Field = 2;
+
+Field[3] = MathEval;
+Field[3].F = "0.2";
+
+Field[4] = Restrict;
+Field[4].IField = 3;
+Field[4].FacesList = {2};
+Field[4].EdgesList = {12, 15};
+
+Field[5] = MathEval;
+Field[5].F = "1";
+
+Field[6] = Restrict;
+Field[6].IField = 5;
+Field[6].EdgesList = {26, 27, 28};
+Field[6].FacesList = {1};
+
+Field[7] = Min;
+Field[7].FieldsList = {2, 4, 6};
+Background Field = 7;
---
+++ +Test/DRIVE_DCExternalMesh.m
@@ -0,0 +1,129 @@
+% Script for setting up the 2.5D DC problem.
+%
+% Problem in 3D:
+%      x = [x, y, z]
+%   \phi = \phi(x)
+%           
+%   -\div(\sigma\grad(\phi)) = I \dirac(x_0) in Omega
+%                       \phi = 0             at d_Omega_1 (earth interior)
+%               d_\phi / d_n = 0             at d_Omega_2 (surface)
+%
+% Problem in 2.5D (at z = 0!):
+%       x = [x, y]
+%   \phi' = \phi'(x, k_Z)
+%
+%   -\div(\sigma\grad(\phi')) + k_z^2 \sigma \phi' = I/2 \dirac(x_0)
+%                    \phi' = 0             at d_Omega_1 (earth interior)
+%            d_\phi' / d_n = 0             at d_Omega_2 (surface)
+%
+% such that
+%    \phi = 2/pi \int_{0}^{\inf} \phi' dk_z
+%
+% 2.5D Variational problem:
+%   a(u,v,k_z) = \int_Omega \grad(\phi') * \sigma \grad(v) + ...
+%                k_z^2 \int_Omega \phi' * \sigma v
+%   f(v)        = I \int_{Omega} \dirac(x_0) v
+%
+% Numerical integration over wavenumbers:
+%   \phi = \sum_{l = 1}^{N} w_l \phi'(k_{z,l})
+%
+% Coordinate system (Kartesian):
+%  0,0 ------> 
+%      |      x
+%      |
+%      |
+%      v
+%       y
+%
+% REMARKS
+%   The 2.5D approach uses the bahavior of a known analytic solution
+%   (e.g. point source in 3D half-space) to set up the numerical 
+%   integration of the seperate 2D solutions (including choice of 
+%   wavenumbers and weights) which forms the 2.5D solution.
+%   This can only act as an approximation for the solution of an arbitrary 
+%   shaped underground!
+%   -> I.e. the solutions should be veryfied by comparing them to a full 3D
+%   simulation.
+%   Furthermore, this implies that only one single point source can be 
+%   treated by this approach such that multi-pole arrangements have to be 
+%   simulated by adding up solutions for several sources.
+%
+%   2.5D approach derived from:    Dey A., Morrison H.F.; 1979
+%   num. integration derived from: Ralph-Uwe Börner (pers. communication)
+%                                  Bing Z.; 1998 (Dissertation)
+%                                  Xu S.; 2000
+
+%% Set up script.
+
+% Clean up and set verbosity.
+kill();
+warning('on');
+verbosity = pick(2, false, true);
+
+%% Set up disctrete DC fwd problem.
+
+% Define type of numerical integration approach.
+FT_type = pick(1, 'Boerner', 'Bing', 'Xu');
+
+% Define source and receiver locations at earth's surface.
+% (With an arbitrary topography)
+TX.type = 'point_exact';
+TX.coo = [1, 1];
+TX.val = 1;
+RX.coo = [0, 0; 0, 5];
+
+% Define mesh.       
+mesh_type = 'gmsh_load';
+file_name = [pwd, '/+Test/', '2D_circ_complex.msh'];
+
+% Set up boundary conditions.
+% Note: ymin denotes earth's surface.
+bnd.type = {'dirichlet', 'neumann'};
+bnd.val = {{[];  0; []}, ... % 1 for Dirichlet
+           { 0; []; 0}}; ... % 2 for Neumann
+bnd.name = {'surface', 'subsurface', 'tunnel'};
+bnd.quad_ord = 1;
+
+%% Set up FEM.
+
+% Define number of grid refinements.
+refinement = 1;
+
+% Set order of Lagrange elements.
+FE_order = pick(2, 1, 2);
+
+% Summarize parameter.
+fwd_params = struct();
+fwd_params.TX = TX;
+fwd_params.RX = RX;
+fwd_params.bnd = bnd;
+fwd_params.FT_type = FT_type;
+fwd_params.FE_order = FE_order;
+fwd_params.ref = refinement;
+clear('TX', 'RX', 'bnd', 'FT_type', 'FE_order', ...
+      'refinement');
+  
+%% Set up mesh.
+    
+mesh = Mesh.initMesh(mesh_type, ...
+    'name', file_name, ...
+    'ref', fwd_params.ref, ...
+    'verbosity', verbosity, ...
+    'TX', fwd_params.TX.coo, ...
+    'RX', fwd_params.RX.coo);
+
+%% Assemble 2.5D DC problem.
+
+[fe, sol, FT_info] = App_DC.assembleDC25D(mesh, fwd_params, verbosity);
+
+%% Solve 2.5D DC problem.
+
+u_FE = App_DC.solveDC25D(fe, sol, FT_info, verbosity);
+
+%% Visualize solution.
+
+Plot.plotMesh(mesh, mesh.params);
+Plot.plotSolution(fe, mesh, u_FE);
+Plot.plotSolution(fe, mesh, u_FE);
+view(2);
+axis('equal');
