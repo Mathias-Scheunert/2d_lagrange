44b924e6237f14bc9502dd50ed16f9be4cd907f2
Mathias Scheunert <mathias.scheunert.geophysik@gmail.com>
2018-10-08 15:30:38 +0200

    Add assembling of RT divergence and mass matrices
        - mass: adapt Lagrange mass matrix assembling
        - div: adapt Lagrange RHS vector assembling
        - Attention: comparison to existing implementations provide equal sparsity pattern and absolute values of the matrices entries BUT there are still differences in sign which NEED TO BE DEBUGGED

--- +FeRT/assembleDiv.m
+++ +FeRT/assembleDiv.m
@@ -1,18 +1,23 @@
-function [S, TS] = assembleDiv(fe, mesh, param, verbosity)
+function D = assembleDiv(fe, mesh, verbosity)
     % Assembles the sparse divergence operator matrix.
     %
-    % TODO: implement.
+    % continuous:
+    %  f(v) = \int_Omega \div u d(x,y) = ...
+    % Galerkin approx. 
+    % (i.e. piece-wise evaluation w.r.t. simplices including the  
+    %     numerical quadrature approx for integral evaluation and coord.
+    %     shift to reference simplex):
+    %   \sum_k \sum_l ( w_l q_k div(\phi_i({x,y}_l)) 1/\det(B_k) ) 
+    %   * abs(\det(B_k))
+    % with
+    %   q_k = 1 for all parameters in the triangle k and
+    %   q_k = 0 elsewhere
     %
-    % a(u,v) = \int_Omega param \grad(u) * \param(x,y) \grad(v) d(x,y) = ...
-    %   \sum_k param_k \abs(\det(B_k)) \sum_l ( w_l ...
-    %       \sum_i u_i B_k^(-1) \grad(\phi_i({x,y}_l)) * ...
-    %       \sum_j u_j B_k^(-1) \grad(\phi_j({x,y}_l))
-    %                              )
-    % k   - num simplices
-    % l   - num quadrature nodes
-    % j,i - num basis functions    
+    % k - num simplices
+    % l - num quadrature nodes
+    % i - num basis functions    
     %
-    % Using elemente-wise procedure to set up the global mass matrix.
+    % Using elemente-wise procedure to set up the global divergence matrix.
     %
     % SYNTAX
     %   D = assembleDiv(fe, mesh, param, verbosity)
@@ -21,17 +26,20 @@ function [S, TS] = assembleDiv(fe, mesh, param, verbosity)
     %   fe    ... Struct, including all information to set up Lagrange FE.
     %   mesh  ... Struct, containing mesh information, i.e. coordinates
     %             of vertices and its relation to the triangles and edges.
-    %   param      ... Vector of constant cell parameter values.
     %
     % OPTIONAL PARAMETER
     %   verbosity ... Logical, denoting if current status should be
     %                 printed.
     %
     % OUTPUT PARAMETER
-    %   S  ... Matrix, representing the stiffness part of the
+    %   D  ... Matrix, representing the divergence part of the
     %          variational formulation.
-    %   TS ... 3-way-tensor, representing the parameter independent
-    %          derivative of S w.r.t. the param vector.
+    %
+    % REMARKS
+    %   As the basis (q_k) of the parameter space is 1 for all points
+    %   within a triangle, i.e. for all quadrature nodes used within 
+    %   assembling loop, it is not incorporated in the code below.
+    %   TODO: Implement in order to have a accurate/consistent description.
     
     %% Check input.
     
@@ -39,90 +47,61 @@ function [S, TS] = assembleDiv(fe, mesh, param, verbosity)
         'fe - struct, including Lagrange reference element info , expected.');
     assert(isstruct(mesh) && all(isfield(mesh, {'cell2cord', 'maps'})), ...
         'mesh - appended struct, containing cell2cord info, expected.');
-    assert(isvector(param) && length(param) == size(mesh.cell2vtx, 1), ...
-        'param - Vector of constant cell parameter values, expected.');
-    if nargin < 4
+    if nargin < 3
         verbosity = false;
     else
         assert(islogical(verbosity), ...
             'verbosity - logical, denoting if status should be printed, expected');
     end
 
-    %% Assemble stiffness matrix.
+    %% Assemble divergence matrix.
 
     if verbosity
-       fprintf('Assemble stiffness matrix ... '); 
+       fprintf('Assemble divergence matrix ... '); 
     end
-    
+       
     % Get common sizes.
     n_cell = fe.sizes.cell;
     n_DOF_glob = fe.sizes.DOF;
-    n_DOF_loc = fe.sizes.DOF_loc;
-    n_quad_point = fe.sizes.quad_point;
-    n_entry_loc = n_DOF_loc^2;
+    n_entry_loc = fe.sizes.DOF_loc;
     
     % Extract quadrature info from struct.
     gauss_cords = fe.quad.nodes;
-    gauss_weights = num2cell(fe.quad.weights);
-    
+    gauss_weights = num2cell(fe.quad.weights.');
+        
     % Initialize index and value vector for sparse matrix assembling.
-    [i, j, s, s_TS] = deal(zeros(n_cell * n_entry_loc, 1));
+    [j, i, s] = deal(zeros(n_cell * n_entry_loc, 1));
     
     % Set up recurring quantity.
-    % Evaluate gradients of basis functions for all Gauss quadrature nodes 
-    % on the reference simplex.
-    grad_quad_eval_loc = arrayfun(@(x,y) ...
-            {fe.base.grad_Phi(x, y)}, gauss_cords(:,1), gauss_cords(:,2));      
-    term2 = cell2mat(grad_quad_eval_loc);
-    
-    % Iterate over all simplices.
-    for ii = 1:n_cell
-        % Incorporate inverse of mapping 
-        term1 = kron(eye(n_quad_point, n_quad_point), mesh.maps{ii}.BinvT);
-        prod = term1 * term2;
-        grad_quad_eval = mat2cell(prod, 2 + zeros(n_quad_point, 1), n_DOF_loc);
-
-        % Set up kernel for integral (quadrature summation).
-        % By multiplying the vector of basis function gradients by itselfe
-        % using the outer (tensor / dyadic) product the 
-        % n_DOF_loc x n_DOF_loc local stiffness matrix can be obtained in
-        % only one step.
-        % (note: base_quad_eval is defined as row vector of gradients)
-        quad_kern = cellfun(@(x, y) {y * (x.' * x)}, ...
-            grad_quad_eval, gauss_weights);
+    % Get divergence of basis functions for all quadrature nodes referred 
+    % to the reference simplex.
+    basis_eval = arrayfun(@(x,y) {fe.base.div_Phi(x, y)}, ...
+        gauss_cords(:,1), gauss_cords(:,2)).';
         
-        % Apply quadrature summation of the integral over the current
-        % simplex.
-        % As integral is referred to the reference simplex, the
-        % Jacobi-determinat has to be incorporated.
-        s_loc = abs(mesh.maps{ii}.detB) * sum(cat(3, quad_kern{:}), 3);
-               
+    % Iterate over all simplices.
+    for ii = 1:n_cell            
+        % Set up kernel for integral (quadrature summation).
+        % By multiplying the vector of basis functions with the
+        % reference/source function.
+        quad_kern = cellfun(@(x, y) {x * y * 1/mesh.maps{ii}.detB;}, ...
+            basis_eval, gauss_weights);
+        
+        % Evaluate numerical integration and incorporate the norm of the 
+        % Jacobi-determinat due to mapping back from reference simplex to 
+        % global coordinates.
+        m_loc = abs(mesh.maps{ii}.detB) * sum(cat(3, quad_kern{:}), 3);
+                  
         % Fill up index and value vectors.
-        cur_DOF_map = fe.DOF_maps.cell2DOF{ii};
-        [i_loc, j_loc] = ndgrid(cur_DOF_map);
+        j_loc = fe.DOF_maps.cell2DOF{ii};
         glob_idx_start = ((ii-1) * n_entry_loc) + 1;
         glob_idx_end = glob_idx_start + n_entry_loc - 1;
-        i(glob_idx_start:glob_idx_end) = i_loc(:);
         j(glob_idx_start:glob_idx_end) = j_loc(:);
-        % Combine constant local cell parameter with the current kernel.
-        s(glob_idx_start:glob_idx_end) = param(ii) * s_loc(:);
-        % Ignore this parameter value for tensor assembling.
-        s_TS(glob_idx_start:glob_idx_end) = s_loc(:);
+        i(glob_idx_start:glob_idx_end) = ii + (0 * j_loc(:));
+        s(glob_idx_start:glob_idx_end) = m_loc(:);
     end
     
-    % Create sparse matrix from index and value vectors.
-    % Note, values belonging to the same index pair are automatically
-    % summed up by sparse().
-    S = sparse(i, j, s, n_DOF_glob, n_DOF_glob);
-    
-    % Create sparse 3-way-tensor from index and value vectors.
-    % Note, that this quantity is independent of the parameter vector. The
-    % stiffness matrix can be obtained by a sparse tensor-times-vector 
-    % multiplication:
-    %   S = ttv(TS, param, 3);
-    size_TS = [n_DOF_glob, n_DOF_glob, fe.sizes.cell];
-    index_TS = [i, j, kron((1:fe.sizes.cell).', ones(n_entry_loc, 1))];
-    TS = Tensor.Tensor3Coord(size_TS, index_TS, s_TS);
+    % Create sparse rhs vector.
+    D = sparse(i, j, s, n_cell, n_DOF_glob);
     
     if verbosity
        fprintf('done.\n'); 
--- +FeRT/assembleMass.m
+++ +FeRT/assembleMass.m
@@ -1,13 +1,18 @@
-function [M, TM] = assembleMass(fe, mesh, param, verbosity)
+function M = assembleMass(fe, mesh, verbosity)
     % Assembles the sparse mass matrix.
     %
-    % TODO: implement.
+    % continuous:
+    % a(u) = \int_Omega |u|^2 d(x,y) = ...
+    % Galerkin approx. 
+    % (i.e. piece-wise evaluation w.r.t. simplices including the  
+    %     numerical quadrature approx for integral evaluation and coord.
+    %     shift to reference simplex):
+    %   \sum_k
+    %       \sum_l ( w_l ...
+    %           (\phi_i({x,y}_l)^T B_k^T) * (B_k \phi_j({x,y}_l))
+    %              ) * 1/\det(B_k)^2
+    %   * \abs(\det(B_k))
     %
-    % a(u,v) = \int_Omega u * \param(x,y) v d(x,y) = ...
-    %   \sum_k param_k \abs(\det(B_k)) \sum_l ( w_l ...
-    %       \sum_i u_i B_k^(-1) \phi_i({x,y}_l) * ...
-    %       \sum_j u_j B_k^(-1) \phi_j({x,y}_l)
-    %                              )
     % k   - num simplices
     % l   - num quadrature nodes
     % j,i - num basis functions  
@@ -15,13 +20,12 @@ function [M, TM] = assembleMass(fe, mesh, param, verbosity)
     % Using elemente-wise procedure to set up the global mass matrix.
     %
     % SYNTAX
-    %   M = assembleMass(fe, mesh, param[, verbosity])
+    %   M = assembleMass(fe, mesh[, verbosity])
     %
     % INPUT PARAMETER
     %   fe    ... Struct, including all information to set up Lagrange FE.
     %   mesh  ... Struct, containing mesh information, i.e. coordinates
     %             of vertices and its relation to the triangles and edges.
-    %   param      ... Vector of constant cell parameter values.
     %
     % OPTIONAL PARAMETER
     %   verbosity ... Logical, denoting if current status should be
@@ -30,8 +34,6 @@ function [M, TM] = assembleMass(fe, mesh, param, verbosity)
     % OUTPUT PARAMETER
     %   M  ... Matrix, representing the mass part of the variational
     %          formulation.
-    %   TS ... 3-way-tensor, representing the parameter independent
-    %          derivative of M w.r.t. the param vector.
     
     %% Check input.
     
@@ -39,21 +41,19 @@ function [M, TM] = assembleMass(fe, mesh, param, verbosity)
         'fe - struct, including Lagrange reference element info , expected.');
     assert(isstruct(mesh) && all(isfield(mesh, {'cell2cord', 'maps'})), ...
         'mesh - appended struct, containing cell2cord info, expected.');
-    assert(isvector(param) && length(param) == size(mesh.cell2vtx, 1), ...
-        'param - Vector of constant cell parameter values, expected.');
-    if nargin < 4
+    if nargin < 3
         verbosity = false;
     else
         assert(islogical(verbosity), ...
             'verbosity - logical, denoting if status should be printed, expected');
     end
 
-    %% Assemble stiffness matrix.
+    %% Assemble mass matrix.
 
     if verbosity
        fprintf('Assemble mass matrix ... '); 
     end
-    
+       
     % Get common sizes.
     n_cell = fe.sizes.cell;
     n_DOF_glob = fe.sizes.DOF;
@@ -64,7 +64,7 @@ function [M, TM] = assembleMass(fe, mesh, param, verbosity)
     gauss_weights = num2cell(fe.quad.weights);
         
     % Initialize index and value vector for sparse matrix assembling.
-    [i, j, m, m_TM] = deal(zeros(n_cell * n_entry_loc, 1));
+    [i, j, m] = deal(zeros(n_cell * n_entry_loc, 1));
     
     % Set up recurring quantity.
     % Get basis functions for all quadrature nodes referred to
@@ -73,20 +73,23 @@ function [M, TM] = assembleMass(fe, mesh, param, verbosity)
         gauss_cords(:,1), gauss_cords(:,2));      
         
     % Iterate over all simplices.
-    for ii = 1:n_cell                   
+    for ii = 1:n_cell
+        % Set up fix (related to coordinate transformation) quantity.
+        BkTBk = (mesh.maps{ii}.B * 1/mesh.maps{ii}.detB).' * ...
+                (mesh.maps{ii}.B * 1/mesh.maps{ii}.detB);
+        
         % Set up kernel for integral (quadrature summation).
         % By multiplying the vector of basis functions by itselfe
         % using the outer (tensor / dyadic) product the 
         % n_DOF_loc x n_DOF_loc local mass matrix can be obtained in
         % only one step.
-        % (note: base_quad_eval is defined as row vector of gradients)
-        quad_kern = cellfun(@(x, y) {y * (x.' * x)}, ...
+        quad_kern = cellfun(@(x, y) {y * (x.' * BkTBk * x)}, ...
             quad_eval, gauss_weights);
         
         % Apply quadrature summation of the integral over the current
         % simplex.
         % As integral is referred to the reference simplex, the
-        % Jacobi-determinat has to be incorporated.
+        % norm of the Jacobi-determinat has to be incorporated.
         m_loc = abs(mesh.maps{ii}.detB) * sum(cat(3, quad_kern{:}), 3);
                   
         % Fill up index and value vectors.
@@ -96,25 +99,13 @@ function [M, TM] = assembleMass(fe, mesh, param, verbosity)
         glob_idx_end = glob_idx_start + n_entry_loc - 1;
         i(glob_idx_start:glob_idx_end) = i_loc(:);
         j(glob_idx_start:glob_idx_end) = j_loc(:);
-        % Combine constant local cell parameter with the current kernel.
-        m(glob_idx_start:glob_idx_end) = param(ii) * m_loc(:);
-        % Ignore this parameter value for tensor assembling.
-        m_TM(glob_idx_start:glob_idx_end) = m_loc(:);
+        m(glob_idx_start:glob_idx_end) = m_loc(:);
     end
     
     % Create sparse matrix from index and value vectors.
     % Note, values belonging to the same index pair are automatically
     % summed up by sparse().
     M = sparse(i, j, m, n_DOF_glob, n_DOF_glob);
-    
-    % Create sparse 3-way-tensor from index and value vectors.
-    % Note, that this quantity is independent of the parameter vector. The
-    % mass matrix can be obtained by a sparse tensor-times-vector 
-    % multiplication:
-    %   M = ttv(TM, param, 3);
-    size_TM = [n_DOF_glob, n_DOF_glob, fe.sizes.cell];
-    index_TM = [i, j, kron((1:fe.sizes.cell).', ones(n_entry_loc, 1))];
-    TM = Tensor.Tensor3Coord(size_TM, index_TM, m_TM);
     
     if verbosity
        fprintf('done.\n'); 
