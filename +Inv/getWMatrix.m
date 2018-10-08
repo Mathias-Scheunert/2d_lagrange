5452b22d4c0767ed609df47f8719572712c18a9f
Mathias Scheunert <mathias.scheunert.geophysik@gmail.com>
2018-10-08 15:35:53 +0200

    Add assembling of model parameter weighting matrix
        - W represents the linear map of smoothness regularization, applied on unstructured grids (in FE sens)

---
+++ +Inv/getWMatrix.m
@@ -0,0 +1,40 @@
+function W = getWMatrix(mesh)
+    % Calculates the representation of the smoothness operator in FE space.
+    %
+    % Derivative follows C. Schwarzbach, Finite element based inversion for
+    %   time-harmonic electromagnetic problems, 2013
+    %
+    % SYNTAX
+    %   W = getWMatrix(mesh)
+    %
+    % INPUT PARAMETER
+    %   mesh ... Struct, containing mesh information, i.e. coordinates of
+    %            vertices and its relation to the triangles and edges.
+    %
+    % OUTPUT PARAMETER
+    %   W ... Matrix representation of the smoothness operator acting on 
+    %         the vector of model parameters, i.e. the weak form of the 
+    %         Poisson problem in the space of the piece-wise constant
+    %         parameter (function), represented by a mixed formulation
+    %         (Raviart-Thomas elements).
+   
+    %% Check input.
+    
+    assert(isstruct(mesh) && all(isfield(mesh, {'cell2vtx'})), ...
+        'mesh - appended struct, containing cells info, expected.');
+    
+    %% Set up matricies for Raviart-Thomas element Poisson problem.
+    
+    % Initialize RT finite elemetns.
+    feRT = FeRT.initFiniteElement(mesh, true);
+    
+    % Assemble mass and divergence operator matrices.
+    M = FeRT.assembleMass(feRT, mesh, true);
+    D = FeRT.assembleDiv(feRT, mesh, true);
+    
+    %% Derive weighting matrix.
+    
+    R = chol(M);
+    W = R \ D.';
+end
+
