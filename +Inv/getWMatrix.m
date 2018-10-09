56e852a404cc20ee19b2854d38e9b4ef6f5470fd
Mathias Scheunert <mathias.scheunert@math.tu-freiberg.de>
2018-10-09 10:30:15 +0200

    Remove obsolete verbosity

--- +Inv/getWMatrix.m
+++ +Inv/getWMatrix.m
@@ -26,11 +26,11 @@ function W = getWMatrix(mesh)
     %% Set up matricies for Raviart-Thomas element Poisson problem.
     
     % Initialize RT finite elemetns.
-    feRT = FeRT.initFiniteElement(mesh, true);
+    feRT = FeRT.initFiniteElement(mesh);
     
     % Assemble mass and divergence operator matrices.
-    M = FeRT.assembleMass(feRT, mesh, true);
-    D = FeRT.assembleDiv(feRT, mesh, true);
+    M = FeRT.assembleMass(feRT, mesh);
+    D = FeRT.assembleDiv(feRT, mesh);
     
     %% Derive weighting matrix.
     
