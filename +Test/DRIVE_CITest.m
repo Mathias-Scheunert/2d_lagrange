cc9a8e4d2feae83c1b8e71f32653508c279ff50c
Mathias Scheunert <mathias.scheunert.geophysik@gmail.com>
2018-10-24 10:33:42 +0200

    Include DRIVER files
        - CI test runs the two driver files

--- +Test/DRIVE_CITest.m
+++ +Test/DRIVE_CITest.m
@@ -1,5 +1,3 @@
-fprintf('Hello world!');
-exit(0);
-%result = runtests('tests', 'IncludeSubPackages', true);
-%disp(result.table);
-%exit(any([result.Failed]));
+result = runtests('Test.DRIVE_Convergence', 'Test.DRIVE_Mesh');
+disp(result.table);
+exit(any([result.Failed]));
